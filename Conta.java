7f922b280ade022519fec62acda7e5b830827211
GlobalCoder007 <ramonsoteriodois@gmail.com>
2015-02-27 20:07:47 -0300

    Conta.java
    Modelo de cadastro de contas

---
+++ Conta.java
@@ -0,0 +1,20 @@
+public class Conta{
+	private int codConta;
+	private String nomeConta;
+	
+	public void setcodConta(int codConta){
+		this.codConta = codConta;
+	}
+	
+	public int getcodConta(){
+		return codConta;
+	}
+	
+	public void setConta(int nomeConta){
+		this.nomeConta = nomeConta;
+	}
+	
+	public String getnomeConta(){
+		return nomeConta;
+	}
+}
