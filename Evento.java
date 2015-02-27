b505d5b72470d519208d45ba75c5654778b8fbb4
GlobalCoder007 <ramonsoteriodois@gmail.com>
2015-02-27 20:08:17 -0300

    Create Evento.java

---
+++ Evento.java
@@ -0,0 +1,29 @@
+public class Evento{
+	private int codEvento;
+	private String nomeEvento;
+	private int tipoEvento;
+	
+	public void setcodEvento(int codEvento){
+		this.codEvento = codEvento;
+	}
+	
+	public int getcodEvento(){
+		return codEvento;
+	}
+	
+	public void setEvento(int nomeEvento){
+		this.nomeEvento = nomeEvento;
+	}
+	
+	public String getnomeEvento(){
+		return nomeEvento;
+	}
+	
+	public void settipoEvento(int tipoEvento){
+		this.tipoEvento = tipoEvento;
+	}
+	
+	public int gettipoEvento(){
+		return tipoEvento;
+	}
+}
